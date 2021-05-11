0304ddfa845ed7a503c4b0b78342e859867a1b4a
yilong <yilong2018@gmail.com>
2021-05-11 13:53:41 +0800

    add project loggingandinjection

---
+++ LoggingSamplePlusInjection/LogHelper.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+
+namespace LoggingSamplePlusInjection
+{
+  public abstract class LogBase
+  {
+    public abstract void Log(string Messsage);
+  }
+
+
+  public class LoggerHelper : LogBase
+  {
+    private string CurrentDirectory { get; set; }
+    private string FileName { get; set; }
+    private string FilePath { get; set; }
+
+    public LoggerHelper()
+    {
+      this.CurrentDirectory = Directory.GetCurrentDirectory();
+      this.FileName = "Log.txt";
+      this.FilePath = this.CurrentDirectory + "/" + this.FileName;
+    }
+
+    public override void Log(string Message)
+    {
+      using (System.IO.StreamWriter writer = System.IO.File.AppendText(this.FilePath))
+      {
+        writer.Write("\r\nLog Entry:");
+        writer.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+        writer.WriteLine($" :{Message}");
+        writer.WriteLine("------------------------------------------");
+      }
+    }
+
+  }
+}
---
+++ LoggingSamplePlusInjection/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LoggingSamplePlusInjection
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var logger = new LoggerHelper();
+      logger.Log("易龙");
+      logger.Log("is one the top of the world");
+
+      var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddSingleton<IFooService, FooService>()
+            .AddSingleton<IBarService, BarService>()
+            .BuildServiceProvider();
+    }
+  }
+}
