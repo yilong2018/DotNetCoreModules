7913a4a4af6f0a81d6f98ee6ecb1cd0c136e8fda
yilong <yilong2018@gmail.com>
2021-05-14 17:16:35 +0800

    add diloggingandsending project

---
+++ DILoggingAndSetting/ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleUI
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    static void BuildConfig(IConfigurationBuilder builder)
+    {
+      builder.SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+          .AddJsonFile("appsettings{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Procution"}.json", optional: false, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+          .AddEnvironmentVariables();
+    }
+  }
+}
